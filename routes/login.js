250e1a341b39f53777da6daf7f70e72cc160dcbb
赵旺 <raceZhao@163.com>
2017-11-13 18:11:25 +0800

    更新登录注册功能

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,55 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-	db.query("INSERT INTO `user_table` (`ID`,`username`,`password`) VALUES(0,'zw1','zhaowang123');",(err,data)=>{
-	if(err){
-		console.log('出错了')
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const DB = require('./linkDB');
+const linkDB = DB.db();
+/*注册 */
+router.use('/register',bodyParser.urlencoded({}))
+router.post('/register', (req, res, next)=>{
+	let ReceivedData = req.body;
+	if(!ReceivedData.username){
+		 res.send({'code':'1','data':{},'msg':"用户名为空"});
+	}else if(!ReceivedData.password){
+		res.send({'code':'1','data':{},'msg':"密码为空"});
 	}else{
-		console.log(data)
+		linkDB.query("SELECT * FROM `user_table` WHERE username='"+ ReceivedData.username +"' ;",(err,data)=>{
+			if(err){
+				console.log('出错了')
+			}else{
+				if(data.length===0){
+					linkDB.query("INSERT INTO `user_table` (`ID`,`username`,`password`) VALUES(0,'"+ ReceivedData.username +"','"+ ReceivedData.password +"');",(err,data)=>{
+						if(err){
+							console.log('出错了')
+						}else{
+			  				res.send({'code':'0','data':{},'msg':"成功"});
+						}
+					})
+				}else{
+					res.send({'code':'1','data':{},'msg':"该用户已被注册"});
+				}
+			}
+		})
 	}
-})
-  res.send('respond with a resource');
 });
-
+/*登录*/
+router.use('/login',bodyParser.urlencoded({}))
+router.post('/login',(req,res)=>{
+	let ReceivedData = req.body;
+	console.log(ReceivedData)
+	linkDB.query("SELECT * FROM `user_table` WHERE username='"+ ReceivedData.username +"' ;",(err,data)=>{
+		if(err){
+			console.log('出错了')
+		}else{
+			if(data.length===0){
+				res.send({'code':'1','data':{},'msg':"该用户名未注册"});
+			}else{
+				if(ReceivedData.username==data[0].username&&ReceivedData.password==data[0].password){
+					res.send({'code':'0','data':{'user_id':data[0]['ID']},'msg':"登录成功"});
+				}else{
+					res.send({'code':"1",data:{},'msg':"用户名或密码错误"})
+				}
+			}
+		}
+	})
+})
 module.exports = router;
