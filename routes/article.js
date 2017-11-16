dd191eea90baeff0c3a6f165c71cbe9e80a699c3
赵旺 <raceZhao@163.com>
2017-11-16 16:05:40 +0800

    日常更新

--- routes/article.js
+++ routes/article.js
@@ -36,26 +36,30 @@ router.get("/add_article_class",(req,res)=>{
 /*----------------------------获取分类列表----------------------------------*/
 router.get("/get_article_class",(req,res)=>{
 	let ReceivedData = req.query;
-	linkDB.query("SELECT * FROM `article_table` WHERE user_id="+ ReceivedData['user_id'] +";",(err,data)=>{
-		if(err){
-			console.log("出错了")
-		}else{
-			let articleArr = eval("("+data[0]['article']+')');
-			if(articleArr.length===0){
-				res.send({code:0,data:[],msg:"成功"})
+	if(!ReceivedData['user_id']){
+		res.send({code:1,data:{},msg:"user_id为空"})
+	}else{
+		linkDB.query("SELECT * FROM `article_table` WHERE user_id="+ ReceivedData['user_id'] +";",(err,data)=>{
+			if(err){
+				console.log("出错了")
 			}else{
-				let backData = [];
-				articleArr.forEach((item,index)=>{
-					backData.push({
-						icon_url:item['icon_url'],
-						article_class_name:item['article_class_name'],
-						article_class_id:item['article_class_id']
+				let articleArr = eval("("+data[0]['article']+')');
+				if(articleArr.length===0){
+					res.send({code:0,data:[],msg:"成功"})
+				}else{
+					let backData = [];
+					articleArr.forEach((item,index)=>{
+						backData.push({
+							icon_url:item['icon_url'],
+							article_class_name:item['article_class_name'],
+							article_class_id:item['article_class_id']
+						})
 					})
-				})
-				res.send({code:0,data:[...backData],msg:"成功"})
+					res.send({code:0,data:[...backData],msg:"成功"})
+				}
 			}
-		}
-	})
+		})
+	}
 })
 
 /*----------------------------删除分类列表----------------------------------*/
@@ -63,23 +67,26 @@ router.get("/get_article_class",(req,res)=>{
 {
 	let articleArr,ReceivedData;
 	router.get("/delete_article_class",(req,res,next)=>{
-		ReceivedData = {
-			user_id : 63,
-			article_class_id:1
+		ReceivedData = req.query;
+		if(!ReceivedData['user_id']){
+			res.send({code:1,data:{},msg:"user_id为空"})
+		}else if(!ReceivedData['article_class_id']){
+			res.send({code:1,data:{},msg:"article_class_id为空"})
+		}else{
+			linkDB.query("SELECT * FROM `article_table` WHERE user_id="+ ReceivedData['user_id'] +";", (err,data)=>{
+				if(err){
+					res.send({code:1,data:{},msg:"数据库链接失败"})
+				}else{
+					articleArr = eval("("+data[0]['article']+')');
+					articleArr.forEach((item,index)=>{
+						if(item['article_class_id']==ReceivedData['article_class_id']){
+							articleArr.splice(index,1);
+							next();
+						}
+					});
+				}
+			})
 		}
-		linkDB.query("SELECT * FROM `article_table` WHERE user_id="+ ReceivedData['user_id'] +";", (err,data)=>{
-			if(err){
-				res.send({code:1,data:{},msg:"数据库链接失败"})
-			}else{
-				articleArr = eval("("+data[0]['article']+')');
-				articleArr.forEach((item,index)=>{
-					if(item['article_class_id']==ReceivedData['article_class_id']){
-						articleArr.splice(index,1);
-						next();
-					}
-				});
-			}
-		})
 	});
 	router.get("/delete_article_class",(req,res)=>{
 		articleArr = JSON.stringify(articleArr)
@@ -92,6 +99,105 @@ router.get("/get_article_class",(req,res)=>{
 		})
 	})
 }
-
-
+/*----------------------------新增文章----------------------------------*/
+router.get('/add_article',(req,res)=>{
+	let ReceivedData = req.query;
+	if(!ReceivedData['user_id']){
+			res.send({code:1,data:{},msg:"user_id为空"})
+	}else if(!ReceivedData['article_class_id']){
+		res.send({code:1,data:{},msg:"article_class_id为空"})
+	}else if(!ReceivedData['title']){
+		res.send({code:1,data:{},msg:"title为空"})
+	}else if(!ReceivedData['content']){
+		res.send({code:1,data:{},msg:"content为空"})
+	}else{
+		linkDB.query("SELECT * FROM `article_table` WHERE user_id="+ ReceivedData['user_id'] +";", (err,data)=>{
+			if(err){
+				res.send({code:1,data:{},msg:"数据库链接失败"})
+			}else{
+				articleArr = eval("("+data[0]['article']+')');
+				articleArr.forEach((item,index)=>{
+					if(item['article_class_id']==ReceivedData['article_class_id']){
+						item['article_list'].push(
+							{
+								article_id:item['article_list'].length==0?0:item['article_list'][item['article_list'].length-1]['article_id']+1,
+								title:ReceivedData['title'],
+								content:ReceivedData['content'],
+								look:0
+							}
+						)
+						articleArr = JSON.stringify(articleArr)
+						linkDB.query("UPDATE `article_table` SET article=' "+articleArr+" '  WHERE user_id="+ReceivedData['user_id']+";",(err,updata)=>{
+							if(err){
+								res.send({code:1,data:{},msg:"数据库链接失败"})
+							}else{
+								res.send({code:0,data:{},msg:"成功"});
+							}
+						})
+					}
+				});
+			}
+		})
+	}
+})
+/*----------------------------获取文章详情----------------------------------*/
+router.get('/get_article_detail',(req,res)=>{
+	let ReceivedData = req.query;
+	if(!ReceivedData['user_id']){
+		res.send({code:1,data:{},msg:"user_id为空"});
+	}else if(!ReceivedData['article_id']){
+		res.send({code:1,data:{},msg:"article_id为空"});
+	}else if(!ReceivedData['article_class_id']){
+		res.send({code:1,data:{},msg:"article_class_id为空"});
+	}else{
+		let updateData;
+		linkDB.query("SELECT * FROM `article_table` WHERE user_id=" +ReceivedData['user_id'] + ";",(err,data)=>{
+			if(err){
+				res.send({code:1,data:{},msg:"数据库链接失败"})
+			}else{
+				articleArr = eval("("+data[0]['article']+')');
+				articleArr.forEach((item,index)=>{
+					if(item['article_class_id']==ReceivedData['article_class_id']){
+						updateData = item;
+					}
+				});
+				updateData['article_list'].forEach((item,index)=>{
+					if(item['article_id']==ReceivedData['article_id']){
+						item['look']+=1;
+						articleArr = JSON.stringify(articleArr)
+						linkDB.query("UPDATE `article_table` SET article=' "+articleArr+" '  WHERE user_id="+ReceivedData['user_id']+";",(err,updata)=>{
+							if(err){
+								res.send({code:1,data:{},msg:"数据库链接失败"})
+							}else{
+								res.send({code:0,data:item,msg:"成功"})
+							}
+						})
+					}
+				})
+			}
+		})
+	}
+});
+/*----------------------------获取单条分类所有文章列表----------------------------------*/
+router.get('/get_artile_class_detail',(req,res)=>{
+	let ReceivedData = req.query;
+	if(!ReceivedData['user_id']){
+		res.send({code:1,data:{},msg:"user_id为空"});
+	}else if(!ReceivedData['article_class_id']){
+		res.send({code:1,data:{},msg:"article_class_id为空"});
+	}else{
+		linkDB.query("SELECT * FROM `article_table` WHERE user_id=" +ReceivedData['user_id'] + ";",(err,data)=>{
+			if(err){
+				res.send({code:1,data:{},msg:"数据库链接失败"})
+			}else{
+				articleArr = eval("("+data[0]['article']+')');
+				articleArr.forEach((item,index)=>{
+					if(item['article_class_id']==ReceivedData['article_class_id']){
+						res.send({code:0,data:item['article_list'],msg:"成功"})
+					}
+				});
+			}
+		})
+	}
+});
 module.exports = router;
